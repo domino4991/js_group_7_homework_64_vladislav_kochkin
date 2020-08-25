68d1a15dc5dcf9bbfe6424ceb9f2123502913c27
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-08-25 22:44:20 +0600

    Добавил в навигацию полный пост и редактирование

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,6 +4,8 @@ import {BrowserRouter, Switch, Route} from "react-router-dom";
 import AddPostsPage from "../AddPostsPage/AddPostsPage";
 import Header from "../../components/Header/Header";
 import HomePage from "../HomePage/HomePage";
+import FullPost from "../FullPost/FullPost";
+import EditPostPage from "../EditPostPage/EditPostPage";
 
 const App = () => {
   return (
@@ -11,8 +13,10 @@ const App = () => {
       <BrowserRouter>
           <Header />
           <Switch>
-              <Route path="/" exact component={HomePage} />
+              <Route path="/" exact component={HomePage}/>
               <Route path="/add-post" component={AddPostsPage} />
+              <Route path="/post/:id" exact component={FullPost} />
+              <Route path="/post/:id/edit" component={EditPostPage} />
           </Switch>
       </BrowserRouter>
     </div>
