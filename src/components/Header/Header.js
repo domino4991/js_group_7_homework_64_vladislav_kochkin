b5f1519684ae617473d278da9e8f53abd040062d
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-08-27 14:57:03 +0600

    Добавил иконку лого

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Header.css';
 import Navigation from "./Navigation/Navigation";
+import {GiAnarchy} from "react-icons/gi";
 
 const Header = () => {
     return (
         <header className="Header">
             <div className="container">
                 <div className="Header-inner">
-                    <span className="Header__logo">My blog</span>
+                    <span className="Header__logo"><GiAnarchy /> My blog</span>
                     <Navigation />
                 </div>
             </div>
