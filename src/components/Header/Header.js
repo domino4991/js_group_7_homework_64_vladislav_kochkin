517710c71e16d0e53916ac5bbefbeb72476fb019
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-08-27 17:46:34 +0600

    Поменял лого

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Header.css';
 import Navigation from "./Navigation/Navigation";
-import {GiAnarchy} from "react-icons/gi";
+import {GiJasonMask} from "react-icons/gi";
 
 const Header = () => {
     return (
         <header className="Header">
             <div className="container">
                 <div className="Header-inner">
-                    <span className="Header__logo"><GiAnarchy /> My blog</span>
+                    <span className="Header__logo"><GiJasonMask /> My blog</span>
                     <Navigation />
                 </div>
             </div>
